90ea9c38bb139934198ab48b740c5a25e953ac5a
David M. Syzdek <david@syzdek.net>
2025-09-05 20:30:29 -0800

    tests: adding test-set-debug

---
+++ tests/test-set-debug.c
@@ -0,0 +1,197 @@
+/*
+ *  Network Calculations Library
+ *  Copyright (C) 2025 David M. Syzdek <david@syzdek.net>.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *     1. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *
+ *     3. Neither the name of the copyright holder nor the names of its
+ *        contributors may be used to endorse or promote products derived from
+ *        this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#define __TESTS_NETCALC_SET_DEBUG_C 1
+#include "libnetcalc-tests.h"
+
+///////////////
+//           //
+//  Headers  //
+//           //
+///////////////
+// MARK: - Headers
+
+#include <errno.h>
+#include <assert.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include <netcalc.h>
+
+
+///////////////////
+//               //
+//  Definitions  //
+//               //
+///////////////////
+// MARK: - Definitions
+
+#undef PROGRAM_NAME
+#define PROGRAM_NAME "netcalc-set-debug"
+
+
+//////////////////
+//              //
+//  Prototypes  //
+//              //
+//////////////////
+// MARK: - Prototypes
+
+extern int
+main(
+         int                           argc,
+         char *                        argv[] );
+
+
+/////////////////
+//             //
+//  Variables  //
+//             //
+/////////////////
+#pragma mark - Variables
+
+static int verbose   = 0;
+static int quiet     = 0;
+
+
+/////////////////
+//             //
+//  Functions  //
+//             //
+/////////////////
+// MARK: - Functions
+
+int
+main(
+         int                           argc,
+         char *                        argv[] )
+{
+   int               c;
+   int               opt_index;
+   int               rc;
+   int               pos;
+   netcalc_set_t *   ns;
+   const char *      test_strs[] =
+   {  "2001:4849:e:ffff::/64",
+      "2001:4849:d:f000::/64",
+      "2001:4849:d:ffff::/64",
+      "2001:4849:d:fffe::/64",
+      "2001:4849:d:100::/64",
+      "2001:4849:d:101::/64",
+      "2001:4849:d:100::/56",
+      "2001:4849:d::/48",
+      "2001:4849:d::/56",
+      "2001:4849:d:a::/64",
+      "2001:4849:e:a::/64",
+      "2001:4849:f:ffff::/64",
+      "2001:4849::/64",
+      "2001:4849:d:101::1/128",
+      "::/0",
+      NULL
+   };
+
+   // getopt options
+   static const char *  short_opt = "hqVv";
+   static struct option long_opt[] =
+   {  {"help",             no_argument,       NULL, 'h' },
+      {"quiet",            no_argument,       NULL, 'q' },
+      {"silent",           no_argument,       NULL, 'q' },
+      {"version",          no_argument,       NULL, 'V' },
+      {"verbose",          no_argument,       NULL, 'v' },
+      { NULL, 0, NULL, 0 }
+   };
+
+   while((c = getopt_long(argc, argv, short_opt, long_opt, &opt_index)) != -1)
+   {  switch(c)
+      {  case -1:       /* no more arguments */
+         case 0:        /* long options toggles */
+            break;
+
+         case 'h':
+            printf("Usage: %s [OPTIONS]\n", PROGRAM_NAME);
+            printf("       %s [OPTIONS] <string> <string> ... <string> \n", PROGRAM_NAME);
+            printf("OPTIONS:\n");
+            printf("  -h, --help                print this help and exit\n");
+            printf("  -q, --quiet, --silent     do not print messages\n");
+            printf("  -V, --version             print version number and exit\n");
+            printf("  -v, --verbose             print verbose messages\n");
+            printf("\n");
+            return(0);
+
+         case 'q':
+            quiet++;
+            break;
+
+         case 'V':
+            printf("%s (%s) %s\n", PROGRAM_NAME, PACKAGE_NAME, PACKAGE_VERSION);
+            printf("Written by David M. Syzdek.\n");
+            return(0);
+
+         case 'v':
+            verbose++;
+            break;
+
+         case '?':
+            fprintf(stderr, "Try `%s --help' for more information.\n", PROGRAM_NAME);
+            return(1);
+
+         default:
+            fprintf(stderr, "%s: unrecognized option `--%c'\n", PROGRAM_NAME, c);
+            fprintf(stderr, "Try `%s --help' for more information.\n", PROGRAM_NAME);
+            return(1);
+      };
+   };
+
+   if ((rc = netcalc_set_init(&ns, 0)) != 0)
+   {  printf("%s: %s\n", PROGRAM_NAME, netcalc_strerror(rc));
+      return(1);
+   };
+
+   netcalc_set_debug(ns, NULL);
+
+   for(pos = 0; ((test_strs[pos])); pos++)
+   {  printf("%4u: adding %s ...\n", (unsigned)pos, test_strs[pos]);
+      if ((rc = netcalc_set_add_str(ns, test_strs[pos], NULL, NULL, 0)) != 0)
+         printf("   %s: netcalc_set_add_str(): %s\n", PROGRAM_NAME, netcalc_strerror(rc));
+   };
+
+   printf("netcalc_set_debug():\n");
+   netcalc_set_debug(ns, NULL);
+
+   netcalc_set_free(ns);
+
+   return( 0 );
+}
+
+/* end of source */
